f53d0216dd89acabff0d5485a43d2457547fcfe4
mmoga <matt.moga@gmail.com>
2018-10-08 22:33:04 -0400

    Made some changes and lost track :-\

--- mysite.js
+++ mysite.js
@@ -37,6 +37,40 @@ if (endstr == -1)
   return unescape(document.cookie.substring(offset, endstr));
 }
 
-window.onbeforeunload = function() {
-    NewWindow('http://matthewmogavero.site','mypopup','640','480','center','front');
-};
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 900, function() {
+          // Callback after animation
+          // Must change focus!
+          // Uncomment below to activate focus
+          /* var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }; */
+        });
+      }
+    }
+  });
